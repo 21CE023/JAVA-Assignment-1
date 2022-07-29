d11e49b8e610fe3a96e16652a7edcf9da87230a2
Jyot <21ce023@charusat.edu.in>
2022-07-29 19:00:31 +0530

    Create Assignment_1.java

---
+++ Assignment_1.java
@@ -0,0 +1,22 @@
+/**
+ * Assignment_1
+ */
+
+import java.util.Scanner;
+
+public class Assignment_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        // String str = "example";
+        char[] ch = a.toCharArray();
+        for (int i = 0; i < 2; i++) {
+            if (ch[i] == 'o') {
+                System.out.println("o");
+            } else if (ch[i] == 'z') {
+                System.out.println("z");
+                javax.crypto
+            }
+        }
+    }
+}
